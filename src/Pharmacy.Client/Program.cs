2ec90947202f5f6b5561c8617a138827306fd78e
Daniil <daniilmitkon129@gmail.com>
2026-04-24 00:26:17 +0300

    Реализация системы логирования через делегаты и события

---
+++ src/Pharmacy.Client/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Pharmacy.Client;
+
+public delegate void ApiLogHandler(string message);
+
+class Program
+{
+    static event ApiLogHandler? OnLog;
+
+    static async Task Main()
+    {
+        // Подписка
+        ApiLogHandler consoleLog = msg => Console.WriteLine($"[LOG]: {msg}");
+        ApiLogHandler fileLog = msg => File.AppendAllText("api.log", $"{DateTime.Now}: {msg}\n");
+
+        OnLog += consoleLog;
+        OnLog += fileLog;
+
+        OnLog?.Invoke("Запрос списка лекарств...");
+        // Тут логика HttpClient...
+
+        // Отписка (требование лабы)
+        OnLog -= fileLog;
+        OnLog?.Invoke("Файловое логирование отключено.");
+    }
+}
